640816115fd2d1c203bcd806e42cfeafdde767bb
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-13 15:07:29 -0400

    Create Navigation Graph

---
+++ app/src/main/java/edu/quinnipiac/ser210/githubapp/tmp/tmp.java
@@ -0,0 +1,6 @@
+package edu.quinnipiac.ser210.githubapp.tmp;
+
+/**
+ * Temp to keep this folder available
+ */
+public class tmp {}
--- app/src/main/java/edu/quinnipiac/ser210/githubapp/ui/activities/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubapp/ui/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package edu.quinnipiac.ser210.githubapp;
+package edu.quinnipiac.ser210.githubapp.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import edu.quinnipiac.ser210.githubapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
