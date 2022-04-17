acd115dfee67dd80e3712f4e9ced36be48b5ffe4
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-17 18:58:33 -0400

    SIGN IN

--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/activities/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.quinnipiac.ser210.githubchat.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OAuthProvider;
 
 import edu.quinnipiac.ser210.githubchat.R;
 
@@ -14,14 +17,18 @@ public class MainActivity extends AppCompatActivity {
     https://firebase.google.com/docs/auth/android/github-auth
      */
 
-    private FirebaseAuth mAuth;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.useEmulator("10.0.2.2",9099);
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+//        FirebaseAuth.getInstance().useEmulator("10.0.2.2",9099);
+        FirebaseAuth.getInstance().useAppLanguage();
+
+        FirebaseAuth.getInstance().startActivityForSignInWithProvider(this, OAuthProvider.newBuilder("github.com").build());
+//        mAuth.useEmulator("10.0.2.2",9099);
     }
 }
--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/fragments/LoginFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/fragments/LoginFragment.java
@@ -2,65 +2,72 @@ package edu.quinnipiac.ser210.githubchat.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OAuthProvider;
 
 import edu.quinnipiac.ser210.githubchat.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LoginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class LoginFragment extends Fragment implements OnSuccessListener<AuthResult>, OnFailureListener, View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public LoginFragment() {
-        // Required empty public constructor
-    }
+    private OAuthProvider.Builder builder;
+    private Task<AuthResult> pendingResultTask;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     *
-     * @return A new instance of fragment LoginFragment.
+    /*
+    https://firebase.google.com/docs/auth/android/github-auth
      */
-    // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        builder = OAuthProvider.newBuilder("github.com");
+
+        pendingResultTask = FirebaseAuth.getInstance().getPendingAuthResult();
+        if(pendingResultTask != null) {
+            pendingResultTask.addOnFailureListener(this).addOnSuccessListener(this);
         }
+
+        return inflater.inflate(R.layout.fragment_login,container,false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.button_login_github).setOnClickListener(this);
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+
+    @Override
+    public void onSuccess(AuthResult authResult) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
