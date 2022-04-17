bf1621c6f3ef7aaa6e8f9136b11c7cf68728eee6
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-17 18:19:36 -0400

    OH, THINGS

--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/activities/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/activities/MainActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import edu.quinnipiac.ser210.githubchat.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    https://firebase.google.com/docs/auth/android/github-auth
+     */
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.useEmulator("10.0.2.2",9099);
     }
 }
