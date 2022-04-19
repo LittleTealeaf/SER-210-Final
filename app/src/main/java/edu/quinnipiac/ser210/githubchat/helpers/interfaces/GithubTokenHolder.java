f28cf43a1919d52cb4ab8169b1e1660834ccab29
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-19 13:20:30 -0400

    Removed comments

---
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/helpers/interfaces/GithubTokenHolder.java
@@ -0,0 +1,5 @@
+package edu.quinnipiac.ser210.githubchat.helpers.interfaces;
+
+public interface GithubTokenHolder {
+    String getGithubToken();
+}
--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/activities/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/activities/MainActivity.java
@@ -3,23 +3,21 @@ package edu.quinnipiac.ser210.githubchat.ui.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.OAuthCredential;
-import com.google.firebase.auth.OAuthProvider;
 
 import java.util.Objects;
 
 import edu.quinnipiac.ser210.githubchat.R;
+import edu.quinnipiac.ser210.githubchat.helpers.interfaces.GithubTokenHolder;
 import edu.quinnipiac.ser210.githubchat.ui.fragments.LoginFragment;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.LoginFragmentListener {
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginFragmentListener, GithubTokenHolder {
 
-    private String githubAPIToken;
+    private String githubToken;
 
     /*
     https://firebase.google.com/docs/auth/android/github-auth
@@ -33,15 +31,16 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
         setContentView(R.layout.activity_main);
 
         navController = Navigation.findNavController(this,R.id.nav_host_fragment);
-
-
-
-//        mAuth.useEmulator("10.0.2.2",9099);
     }
 
     @Override
     public void onLogin(AuthResult authResult) {
-        githubAPIToken = ((OAuthCredential) Objects.requireNonNull(authResult.getCredential())).getAccessToken();
+        githubToken = ((OAuthCredential) Objects.requireNonNull(authResult.getCredential())).getAccessToken();
         navController.navigate(R.id.action_loginFragment_to_debugChatFragment);
+    }
+
+    @Override
+    public String getGithubToken() {
+        return githubToken;
     }
 }
