527d9eab8be085eb5a4ee7984e6a582bdf1dd244
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-04-25 19:08:14 -0400

    Added FAB that navigates to CreateChatFragment

--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/fragments/HomeFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/fragments/HomeFragment.java
@@ -5,18 +5,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import edu.quinnipiac.ser210.githubchat.R;
 import edu.quinnipiac.ser210.githubchat.github.GithubWrapper;
 import edu.quinnipiac.ser210.githubchat.github.async.FetchGithubUserTask;
 import edu.quinnipiac.ser210.githubchat.github.dataobjects.GithubUser;
 
-public class HomeFragment extends Fragment implements FetchGithubUserTask.Listener {
+public class HomeFragment extends Fragment implements FetchGithubUserTask.Listener, View.OnClickListener {
 
 
     @Nullable
@@ -30,13 +33,22 @@ public class HomeFragment extends Fragment implements FetchGithubUserTask.Listen
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        GithubWrapper.fromObject(requireActivity()).fetchGithubUser("LittleTealeaf", this);
+
+        ((FloatingActionButton) view.findViewById(R.id.home_fab_create)).setOnClickListener(this);
+
 
         super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public void onFetchGithubUser(GithubUser user) {
-        ((TextView) requireView().findViewById(R.id.fragment_home_text_test)).setText(user.getLogin());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.home_fab_create) {
+            Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment_to_createChatFragment);
+        }
     }
 }
