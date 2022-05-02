fc62af2fed60432ef9819fa39d1d9596576a0aee
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-02 18:20:02 -0400

    Remove Deprecated Files

--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/adapters/viewholders/MessageViewHolder.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/adapters/viewholders/MessageViewHolder.java
@@ -10,7 +10,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.regex.Matcher;
@@ -19,19 +18,15 @@ import java.util.regex.Pattern;
 import edu.quinnipiac.ser210.githubchat.R;
 import edu.quinnipiac.ser210.githubchat.firebase.dataobjects.Message;
 import edu.quinnipiac.ser210.githubchat.github.GithubWrapper;
-import edu.quinnipiac.ser210.githubchat.github.dataobjects.GithubAttachment;
 import edu.quinnipiac.ser210.githubchat.github.dataobjects.GithubUser;
-import edu.quinnipiac.ser210.githubchat.github.listeners.OnFetchGithubAttachable;
 import edu.quinnipiac.ser210.githubchat.github.listeners.OnFetchGithubUser;
 import edu.quinnipiac.ser210.githubchat.threads.ThreadManager;
-import edu.quinnipiac.ser210.githubchat.ui.adapters.AttachableAdapter;
 import edu.quinnipiac.ser210.githubchat.ui.adapters.AttachmentAdapter;
 import edu.quinnipiac.ser210.githubchat.ui.adapters.MessageAdapter;
 import edu.quinnipiac.ser210.githubchat.ui.util.ImageLoader;
 import edu.quinnipiac.ser210.githubchat.ui.util.OnImageLoaded;
 
-public class MessageViewHolder extends RecyclerView.ViewHolder
-        implements OnFetchGithubUser, OnImageLoaded, OnFetchGithubAttachable, View.OnClickListener {
+public class MessageViewHolder extends RecyclerView.ViewHolder implements OnFetchGithubUser, OnImageLoaded, View.OnClickListener {
 
     private final MessageAdapter adapter;
     private final TextView userView;
@@ -39,15 +34,8 @@ public class MessageViewHolder extends RecyclerView.ViewHolder
     private final TextView timeView;
     private final ImageView avatarView;
     private final AttachmentAdapter attachmentAdapter;
-    private final ListView attachmentListView;
-//    private final AttachableAdapter attachableAdapter;
-//    private final RecyclerView recyclerView;
-
 
     private int channelFetchUser, channelLoadImage;
-
-    @Deprecated
-    private int channelFetchAttachable;
 
     private Message message;
     private GithubUser githubUser;
@@ -59,21 +47,11 @@ public class MessageViewHolder extends RecyclerView.ViewHolder
         messageView = itemView.findViewById(R.id.list_message_text_message);
         avatarView = itemView.findViewById(R.id.list_message_imageview_avatar);
         timeView = itemView.findViewById(R.id.list_message_text_time);
-        attachmentListView = itemView.findViewById(R.id.list_message_listview_attachments);
-
+        ListView attachmentListView = itemView.findViewById(R.id.list_message_listview_attachments);
         attachmentAdapter = new AttachmentAdapter(messageAdapter.getContext());
         attachmentListView.setAdapter(attachmentAdapter);
 
-
-//        attachableAdapter = new AttachableAdapter(messageAdapter.getContext());
-
-
-
         avatarView.setOnClickListener(this);
-
-//        recyclerView = ((RecyclerView) itemView.findViewById(R.id.list_message_recyclerview_attachable));
-//        recyclerView.setLayoutManager(new LinearLayoutManager(adapter.getContext()));
-//        recyclerView.setAdapter(attachableAdapter);
     }
 
     public void onBindMessage(Message message) {
@@ -82,9 +60,7 @@ public class MessageViewHolder extends RecyclerView.ViewHolder
         userView.setText(message.getSender());
         messageView.setText(message.getMessage());
         avatarView.setVisibility(View.INVISIBLE);
-//        recyclerView.setVisibility(View.GONE);
-        channelFetchAttachable = ThreadManager.registerChannel();
-//        attachableAdapter.clearItems();
+
         channelFetchUser = GithubWrapper.from(adapter.getContext()).startFetchGithubUser(message.getSender(), this);
 
         int channel = ThreadManager.registerChannel();
@@ -99,6 +75,13 @@ public class MessageViewHolder extends RecyclerView.ViewHolder
             int number = Integer.parseInt(matcher.group().substring(1));
             githubWrapper.startFetchGithubAttachable(adapter.getRepoName(), number, attachmentAdapter, channel);
         }
+    }
+
+    public void updateTime() {
+         /*
+        Source: https://stackoverflow.com/a/69348167/12206859
+         */
+        timeView.setText(DateUtils.getRelativeTimeSpanString(message.getSendTime()));
     }
 
     @Override
@@ -120,15 +103,6 @@ public class MessageViewHolder extends RecyclerView.ViewHolder
         }
     }
 
-    @Deprecated
-    @Override
-    public void onFetchGithubAttachable(GithubAttachment attachable, int channel) {
-        if (channel == channelFetchAttachable && attachable != null) {
-//            attachableAdapter.addAttachable(attachable);
-//            recyclerView.setVisibility(View.VISIBLE);
-        }
-    }
-
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.list_message_imageview_avatar && githubUser != null) {
@@ -136,13 +110,4 @@ public class MessageViewHolder extends RecyclerView.ViewHolder
             adapter.getContext().startActivity(intent);
         }
     }
-
-    public void updateTime() {
-         /*
-        Source: https://stackoverflow.com/a/69348167/12206859
-         */
-        timeView.setText(DateUtils.getRelativeTimeSpanString(message.getSendTime()));
-
-    }
-
 }
