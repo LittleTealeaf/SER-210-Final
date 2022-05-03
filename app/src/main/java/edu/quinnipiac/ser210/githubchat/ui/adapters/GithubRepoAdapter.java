26a1b57e8fc5951b9fbb9fa76959a6535d71e755
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-03 15:26:29 -0400

    Reformat

--- app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/adapters/GithubRepoAdapter.java
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/adapters/GithubRepoAdapter.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 
 import edu.quinnipiac.ser210.githubchat.R;
 import edu.quinnipiac.ser210.githubchat.database.DatabaseWrapper;
-import edu.quinnipiac.ser210.githubchat.database.dataobjects.ChatRoom;
 import edu.quinnipiac.ser210.githubchat.github.GithubWrapper;
 import edu.quinnipiac.ser210.githubchat.github.dataobjects.GithubRepo;
 import edu.quinnipiac.ser210.githubchat.threads.ThreadManager;
@@ -25,9 +24,7 @@ import edu.quinnipiac.ser210.githubchat.ui.util.FilterableList;
 /**
  * @author Thomas Kwashnak
  */
-public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoViewHolder>
-        implements FilterableList.ChangeListener<GithubRepo> {
-
+public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoViewHolder> implements FilterableList.ChangeListener<GithubRepo> {
 
     private final Context context;
     private final LayoutInflater inflater;
@@ -47,24 +44,25 @@ public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoViewHolder
         filterableList = new FilterableList<>(this::filterRepo, this);
 
         final DatabaseWrapper databaseWrapper = DatabaseWrapper.from(context);
-        ThreadManager.startThread(() -> GithubWrapper.from(context).fetchGithubRepoList(null)
+        ThreadManager.startThread(() -> GithubWrapper
+                .from(context)
+                .fetchGithubRepoList(null)
                 .stream()
                 .filter((repo) -> databaseWrapper.getChatRoom(repo.getFullName()) == null)
-                .collect(Collectors.toList()), (items,index) -> items.forEach(filterableList::addItem));
-
+                .collect(Collectors.toList()), (items, index) -> items.forEach(filterableList::addItem));
     }
 
     private boolean filterRepo(GithubRepo repo, String filterString) {
 
-       if(!filterString.equals("")) {
-           String[] filters = filterString.toLowerCase(Locale.ROOT).split(" ");
+        if (!filterString.equals("")) {
+            String[] filters = filterString.toLowerCase(Locale.ROOT).split(" ");
 
-           for (String filter : filters) {
-               if (!repo.getFullName().toLowerCase().contains(filter)) {
-                   return false;
-               }
-           }
-       }
+            for (String filter : filters) {
+                if (!repo.getFullName().toLowerCase().contains(filter)) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
@@ -95,7 +93,6 @@ public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoViewHolder
         return selected;
     }
 
-
     public void filterItems(String filterText) {
         filterableList.setFilter(filterText);
         this.filter = filterText;
@@ -110,14 +107,13 @@ public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoViewHolder
         }
     }
 
-
     @Override
     public void onItemAdded(GithubRepo item) {
-        for(int i = 0; i < githubRepos.size(); i++) {
+        for (int i = 0; i < githubRepos.size(); i++) {
             GithubRepo item2 = githubRepos.get(i);
-            if(item2.getFullName().toLowerCase().charAt(0) > item.getFullName().toLowerCase().charAt(0) && item2.getFullName().charAt(0) > item.getFullName().charAt(0)){
-                githubRepos.add(i,item);
-                notifyItemInserted(i+1);
+            if (item2.getFullName().toLowerCase().charAt(0) > item.getFullName().toLowerCase().charAt(0) && item2.getFullName().charAt(0) > item.getFullName().charAt(0)) {
+                githubRepos.add(i, item);
+                notifyItemInserted(i + 1);
                 return;
             }
         }
@@ -128,11 +124,10 @@ public class GithubRepoAdapter extends RecyclerView.Adapter<GithubRepoViewHolder
     @Override
     public void onItemRemoved(GithubRepo item) {
         int index = githubRepos.indexOf(item);
-        if(index != -1) {
+        if (index != -1) {
             githubRepos.remove(item);
-            notifyItemRemoved(index+1);
+            notifyItemRemoved(index + 1);
         }
-
     }
 
     public interface OnGithubRepoSelected {
