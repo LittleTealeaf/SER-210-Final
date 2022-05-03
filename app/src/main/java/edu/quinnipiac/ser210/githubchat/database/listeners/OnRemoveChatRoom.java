4e5b25a09fe853c66028128a6b029ec4e34e8672
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-03 12:11:17 -0400

    Deleted Database Listeners

--- app/src/main/java/edu/quinnipiac/ser210/githubchat/database/listeners/OnRemoveChatRoom.java
+++
@@ -1,9 +0,0 @@
-package edu.quinnipiac.ser210.githubchat.database.listeners;
-
-import edu.quinnipiac.ser210.githubchat.database.DatabaseWrapper;
-
-@Deprecated
-public interface OnRemoveChatRoom extends DatabaseWrapper.OnRemoveChatRoom {
-
-    void onRemoveChatRoom(String repoName, int channel);
-}
