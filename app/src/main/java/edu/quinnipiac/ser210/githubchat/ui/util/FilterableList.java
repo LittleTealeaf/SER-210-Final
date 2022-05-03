efb71ecab3b39e583481e44f9b43880adf8baeec
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-05-03 15:25:52 -0400

    Optimized Create Chat listing

---
+++ app/src/main/java/edu/quinnipiac/ser210/githubchat/ui/util/FilterableList.java
@@ -0,0 +1,73 @@
+package edu.quinnipiac.ser210.githubchat.ui.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class FilterableList<T> {
+
+    /*
+    TODO: add list of currently filtered items
+    TODO: implement required methods (
+     */
+
+    private final ChangeListener<T> changeListener;
+    private final Filter<T> filter;
+    private final Map<T,Boolean> items = new HashMap<>();
+
+//    private final List<FilteredItem<T>> items = new LinkedList<>();
+
+    private String filterString = "";
+
+    public FilterableList(Filter<T> filter, ChangeListener<T> changeListener) {
+        this.changeListener = changeListener;
+        this.filter = filter;
+    }
+
+    public void setFilter(String filterString) {
+        this.filterString = filterString;
+        for(T item : items.keySet()) {
+            Boolean current = items.get(item);
+            assert current != null;
+            Boolean newFilter = filter.filterItem(item,filterString);
+            if(newFilter ^ current) {
+                if(newFilter) {
+                    changeListener.onItemAdded(item);
+                } else {
+                    changeListener.onItemRemoved(item);
+                }
+            }
+            items.put(item,filter.filterItem(item,filterString));
+
+        }
+    }
+
+    public void addItem(T item) {
+        if(!items.containsKey(item)) {
+            boolean show = filter.filterItem(item,filterString);
+            items.put(item,show);
+            if(show) {
+                changeListener.onItemAdded(item);
+            }
+        }
+    }
+
+
+    public void removeItem(T item) {
+        if(items.getOrDefault(item, false) == Boolean.TRUE) {
+            changeListener.onItemRemoved(item);
+        }
+        items.remove(item);
+    }
+
+    public interface Filter<T> {
+        boolean filterItem(T item, String filter);
+    }
+
+    public interface ChangeListener<T> {
+        void onItemAdded(T item);
+        void onItemRemoved(T item);
+    }
+}
